464a32cfdf78ef9716b4050902407f127d79c2e8
mrmoizjoyo979 <167323808+mrmoizjoyo979@users.noreply.github.com>
2024-07-20 12:07:07 +0500

    Update app.ts

--- step09b_function_optional_parameter/app.ts
+++ step09b_function_optional_parameter/app.ts
@@ -1,26 +1,14 @@
 
-//Named function with optional parameters
-function buildName(firstName: string, lastName?: string) : string {
-    if (lastName)
-        return firstName + " " + lastName;
-    else
         return firstName;
 }
 
 let result1 = buildName("Bob");  //ok
-let result2 = buildName("Bob", "Adams", "Sr.");  //error, too many parameters
-var result3 = buildName("Bob", "Adams");  //also ok
+let result2 = builvar result3 = buildName("Bob", "Adams");  //also ok
 
 
 
-//anonymous function type with optional parameters
-var buildName1 : (firstName: string, lastName?: string) => string = 
-function(firstName: string, lastName?: string) : string {
-    if (lastName)
-        return firstName + " " + lastName;
-    else
-        return firstName;
-}
-	
-    
-    
+/anonymous function type with optional parameters
+vuildName1 : (firstName: string, lastName?: string) => string = 
+functrstName: string, lastName?: string) : string {
+    if (las
+        return firs
